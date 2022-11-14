525eef88e168c207c68f1a43cf472c98d7f9bd44
sathish.ponnusamy1@gmail.com <Rainbow1a>
2022-11-14 11:55:57 +0000

    Allure report integration to the local test run

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   projectId: 'v4demp',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+//       on('file:preprocessor', webpackPreprocessor);
+       allureWriter(on, config);
+                  return config;
     },
+
+            env: {
+                allureReuseAfterSpec: true
+            }
   },
 });
